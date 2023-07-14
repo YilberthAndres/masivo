c4e11eee173aa1adfa43f4b8580ff6f3188f409a
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-07-14 10:01:22 -0500

    carga

--- mensajeria/static/js/carga/index.js
+++ mensajeria/static/js/carga/index.js
@@ -1,26 +1,30 @@
 $("#miFormulario").submit(function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+    event.preventDefault(); // Evita el envío predeterminado del formulario
+
     var formulario = $(this)[0];
     var formData = new FormData(formulario);
 
-    $.ajax({
-        url: "uploaded",
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function(response) {
-            // Lógica para manejar la respuesta del servidor
-            if (response.success) {
-                console.log(response);
-                alert("Formulario guardado exitosamente");
-            } else {
-                alert("Error al guardar el formulario");
-            }
-        },
-        error: function(xhr, status, error) {
-            // Lógica para manejar el error
-            alert("Error en la solicitud AJAX");
-        }
+    fetch("uploaded", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        // Crea un objeto URL para el blob
+        var url = URL.createObjectURL(blob);
+
+        // Crea un enlace de descarga y dispara automáticamente la descarga
+        var linkDescarga = document.createElement("a");
+        linkDescarga.href = url;
+        linkDescarga.download = "archivo.xlsx";
+        linkDescarga.click();
+
+        // Libera el objeto URL
+        URL.revokeObjectURL(url);
+    })
+    .catch(error => {
+        // Lógica para manejar el error
+        console.log("Error en la solicitud fetch:", error);
     });
 });
+
--- mensajeria/views/carga/carga.py
+++ mensajeria/views/carga/carga.py
@@ -11,7 +11,8 @@ from datetime import datetime
 from django.conf import settings
 import pandas as pd
 import json
-from django.http import JsonResponse
+from django.http import HttpResponse
+from openpyxl import Workbook
 
 @login_required(login_url='signin')
 def index(request):
@@ -58,6 +59,12 @@ def uploaded(request):
 
 
 
+                archivo_excel = generar_archivo_excel(errados, duplicados)
+
+                with open(archivo_excel, "rb") as archivo:
+                    response = HttpResponse(archivo.read(), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                    response["Content-Disposition"] = f"attachment; filename={archivo_excel}"
+                    return response
 
 
                 # Devolver el arreglo como respuesta JSON
@@ -68,3 +75,27 @@ def uploaded(request):
 
     # Si no se recibió un archivo válido o no se procesó correctamente, devolver una respuesta de error
     return JsonResponse({'error': 'No se recibió un archivo válido.'})
+
+def generar_archivo_excel(errados, duplicados):
+    # Crear un nuevo libro de trabajo de Excel
+    workbook = Workbook()
+
+    # Crear una hoja de cálculo para los registros errados
+    hoja_errados = workbook.create_sheet(title="Errados")
+    for registro in errados:
+        hoja_errados.append(registro)
+
+    # Crear una hoja de cálculo para los registros duplicados
+    hoja_duplicados = workbook.create_sheet(title="Duplicados")
+    for registro in duplicados:
+        hoja_duplicados.append(registro)
+
+    # Eliminar la hoja de cálculo por defecto "Sheet"
+    del workbook["Sheet"]
+
+    # Guardar el libro de trabajo en un archivo
+    nombre_archivo = "registros.xlsx"
+    workbook.save(nombre_archivo)
+
+    # Devolver el nombre del archivo generado
+    return nombre_archivo
