63e81bea69581ea1262a505998a8f0b2ce23c2a1
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-11-06 19:03:54 -0500

    carga

--- mensajeria/views/carga/carga.py
+++ mensajeria/views/carga/carga.py
@@ -26,6 +26,8 @@ from ...serializers.auth.signin_serializers import SigninSerializers
 from rest_framework.response import Response
 from rest_framework import status
 
+from mensajeria.tasks import my_task
+
 
 class Uploaded(CreateAPIView, ResponseMixin):
     serializer_class = SignupSerializers
@@ -60,8 +62,112 @@ class Uploaded(CreateAPIView, ResponseMixin):
             [str(x) if x is not None and not pd.isna(x) else "" for x in row]
             for row in matriz
         ]
+    
+    def post(self, request, *args, **kwargs):
+        
+        if "archivo_excel" in request.FILES:
+            archivo = request.FILES["archivo_excel"]
+            if archivo.name.endswith((".xls", ".xlsx")):
+                # try:
+                    df = pd.read_excel(archivo, engine="openpyxl")
+                    matriz = df.values.tolist()
+
+                    errados             = []
+                    validos             = []
+                    validos_celular     = []
+                    duplicados          = []
+
+                    num_validos      = 0
+                    num_duplicados  = 0
+                    num_errados     = 0
+
+                    personas_actuales = Personas.objects.all().order_by('telefonowhatsapp')
+                    # 
+
+
+                    validos_actuales = []
+                    for persona in personas_actuales:
+                        validos_actuales.append(persona.telefonowhatsapp)
+
+                    matriz_data = self.get_validar_campos(matriz)
+                    for row in matriz_data:
+                        nombre              = row[0]
+                        segundo_nombre      = row[1]
+                        apellido            = row[2]
+                        segundo_apellido    = row[3]
+                        celular             = row[4]
+                        celular_llamada     = row[5]
+                        documento           = row[7]
+
+
+                        if not isinstance(celular, str):
+                            celular         = str(celular)
+
+                        if not isinstance(celular_llamada, str):
+                            celular_llamada = str(celular_llamada).rstrip('.0')
+
+                        documento       = str(documento).rstrip('.0')
+
+                        persona_new = {
+                                    "nombre"            :   nombre,
+                                    "segundo_nombre"    :   segundo_nombre,
+                                    "apellido"          :   apellido,
+                                    "segundo_apellido"  :   segundo_apellido,
+                                    "celular_whatsapp"  :   celular,
+                                    "celular_llamada"   :   celular_llamada,
+                                    "documento"         :   documento,
+                                    "message"           :   ""
+                                    }
+
+                        
+                        if not self.get_validar_numero(persona_new['celular_whatsapp']):
+                            persona_new['message'] = "Numero de whatsapp invalido."
+                            errados.append(persona_new)
+                            num_errados = num_errados + 1
+                        else:        
+                            validos_celular.sort()
+                            index = self.get_binary_search(validos_celular, celular)
+
+                            if index != -1:
+                                persona_new['message'] = "Numero de whatsapp duplicado en el excel."
+                                duplicados.append(persona_new)
+                                num_duplicados = num_duplicados + 1
+                            else: 
+                                index2 = self.get_binary_search(validos_actuales, celular)
+                                if index2 != -1:
+                                    persona_new['message'] = "Numero de whatsapp duplicado en la base de datos."
+                                    duplicados.append(persona_new)
+                                    num_duplicados = num_duplicados + 1
+                                else: 
+                                    persona_new['message'] = "Datos correctos."
+                                    validos_celular.append(celular)
+                                    validos.append(persona_new)
+                                    num_validos = num_validos + 1
+
+                    data = {
+                        "validos": {"count": num_validos, "data": validos},
+                        "errados": {"count": num_errados, "data": errados},
+                        "duplicados": {"count": num_duplicados, "data": duplicados},
+                    }
+
+                    # result = my_task.delay(duplicados)
+                    # result_value = result.get()
+                    # print("Tarea")
+                    # result = my_task.get()
+                    
+                    self.data = {"status": status.HTTP_200_OK, "data": data, "state": True}
+                # except Exception as e: 
+                #     self.data = {"status": status.HTTP_400_BAD_REQUEST, "data": "Error", "state": False}
+            
+        else:    
+            self.data = {"status": status.HTTP_400_BAD_REQUEST, "data": "Error", "state": False}
+        return Response(self.response_obj)
+    
+
 
     
+class Save(CreateAPIView, ResponseMixin):
+    serializer_class = SignupSerializers
 
     def post_add_person(self, data, user):
 
@@ -87,103 +193,48 @@ class Uploaded(CreateAPIView, ResponseMixin):
         nuevo_registro.save()
 
         return True
-    
+
     def post(self, request, *args, **kwargs):
+        personas        = request.data['destinatarios']
+
+        print(personas)
+        user            = request.user
+        validos         = []
+        num_validos     = 0
+        invalidos       = []
+        num_invalidos   = 0
+
         
-        if "archivo_excel" in request.FILES:
-            archivo = request.FILES["archivo_excel"]
-            if archivo.name.endswith((".xls", ".xlsx")):
-                # try:
-                    df = pd.read_excel(archivo, engine="openpyxl")
-                    matriz = df.values.tolist()
+        for persona in personas:
 
-                    errados             = []
-                    validos             = []
-                    validos_celular     = []
-                    duplicados          = []
-
-                    personas_actuales = Personas.objects.all().order_by('telefonowhatsapp')
-                    user = request.user
-                    user = User.objects.get(id=user.id)
-
-                    validos_actuales = []
-                    for persona in personas_actuales:
-                        validos_actuales.append(persona.telefonowhatsapp)
-
-                    matriz_data = self.get_validar_campos(matriz)
-                    for row in matriz_data:
-                        nombre              = row[0]
-                        segundo_nombre      = self.get_data_null(row[1])
-                        apellido            = row[2]
-                        segundo_apellido    = row[3]
-                        celular             = row[4]
-                        celular_llamada     = row[5]
-                        documento           = row[7]
+            persona_new = {
+                "nombre"            :   persona["nombre"],
+                "segundo_nombre"    :   persona["segundo_nombre"],
+                "apellido"          :   persona["apellido"],
+                "segundo_apellido"  :   persona["segundo_apellido"],
+                "celular_whatsapp"  :   persona["celular_whatsapp"],
+                "celular_llamada"   :   persona["celular_llamada"],
+                "documento"         :   persona["documento"],
+                }
+            try:
+                self.post_add_person(persona_new, user)
+                validos.append(persona_new)
+                num_validos = num_validos + 1
+            except Exception as e:
+                invalidos.append(persona_new)
+                num_invalidos = num_invalidos + 1
 
 
-                        if not isinstance(celular, str):
-                            celular         = str(celular)
-
-                        if not isinstance(celular_llamada, str):
-                            celular_llamada = str(celular_llamada).rstrip('.0')
-
-                        # if not isinstance(documento, str):
-                        documento       = str(documento).rstrip('.0')
+        data = {
+                "validos"   : {"count": num_validos, "data": validos},
+                "invalidos" : {"count": num_invalidos, "data": invalidos},
+                "error"     : num_invalidos
+            }
 
 
-                        
-                        if not self.get_validar_numero(celular):
-                            errados.append({
-                                    "nombre"            :   nombre,
-                                    "segundo_nombre"    :   segundo_nombre,
-                                    "apellido"          :   apellido,
-                                    "segundo_apellido"  :   segundo_apellido,
-                                    "celular_whatsapp"  :   celular,
-                                    "celular_llamada"   :   celular_llamada,
-                                    "documento"         :   documento
-                                    })
-                        else:
-                            persona_new = {
-                                    "nombre"            :   nombre,
-                                    "segundo_nombre"    :   segundo_nombre,
-                                    "apellido"          :   apellido,
-                                    "segundo_apellido"  :   segundo_apellido,
-                                    "celular_whatsapp"  :   celular,
-                                    "celular_llamada"   :   celular_llamada,
-                                    "documento"         :   documento
-                                    }
-                                                    
-                            validos_celular.sort()
-                            index = self.get_binary_search(validos_celular, celular)
 
-                            if index != -1:
-                                duplicados.append(persona_new)
-                            else: 
-                                index2 = self.get_binary_search(validos_actuales, celular)
-                                if index2 != -1:
-                                    duplicados.append(persona_new)
-                                else: 
-                                    validos_celular.append(celular)
-
-                                    self.post_add_person(persona_new, user)
-                                    validos.append(persona_new)
-
-                    data = {
-                        "validos": validos,
-                        "errados": errados,
-                        "duplicados": duplicados,
-                    }
-                    
-                    self.data = {"status": status.HTTP_200_OK, "data": data, "state": True}
-                # except Exception as e: 
-                #     self.data = {"status": status.HTTP_400_BAD_REQUEST, "data": "Error", "state": False}
-            
-        else:    
-            self.data = {"status": status.HTTP_400_BAD_REQUEST, "data": "Error", "state": False}
+        self.data = {"status": status.HTTP_200_OK, "data": data, "state": True}
         return Response(self.response_obj)
-    
-
-
 
 # @login_required(login_url="signin")
 # def index(request):
