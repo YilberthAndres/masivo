02bfaae56b0a05e02d85903baa5bb17b414ce944
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-11-09 16:08:10 -0500

    url chat

--- mensajeria/views/chat/urls.py
+++ mensajeria/views/chat/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .chat import MenssageLeft
+from .chat import MenssageLeft, MenssageFind, MenssageSend
 
 app_name = "masivo-upload"
 
 urlpatterns = [
-    path("menssage_left/",  MenssageLeft.as_view(), name="menssage_left"),
+    path("message_left/",  MenssageLeft.as_view(), name="message_left"),
+    path("message_find/",  MenssageFind.as_view(), name="message_find"),
+    path("message_send/",  MenssageSend.as_view(), name="message_send"),
 
 ]
