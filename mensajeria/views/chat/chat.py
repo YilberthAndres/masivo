74a2de3964e3189365fcec3ee1b150479c3c0984
Nelson <neldecas12@gmail.com>
2023-11-12 13:13:21 -0500

    .

--- mensajeria/views/chat/chat.py
+++ mensajeria/views/chat/chat.py
@@ -7,7 +7,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.core import serializers
 import json
-from mensajeria.models import Archivos, Destinatarios, Mensajeria, Personas, Conversaciones
+from mensajeria.models import (
+    Archivos,
+    Destinatarios,
+    Mensajeria,
+    Personas,
+    Conversaciones,
+)
 from django.utils import timezone
 from mensajeria.tasks import my_task
 from django.db.models.functions import TruncDate
@@ -18,29 +24,31 @@ import os
 import requests
 from django.core.exceptions import ObjectDoesNotExist
 
-API_KEY_ENV                 = os.getenv('API_KEY')
-ID_WHATSAPP_BUSINESS_ENV    = os.getenv('ID_WHATSAPP_BUSINESS')
-ID_WHATSAPP_NUMBER_ENV      = os.getenv('ID_WHATSAPP_NUMBER')
-API_VERSION_WHATSAPP_ENV    = os.getenv('API_VERSION_WHATSAPP')
+
+API_KEY_ENV = os.getenv("API_KEY")
+ID_WHATSAPP_BUSINESS_ENV = os.getenv("ID_WHATSAPP_BUSINESS")
+ID_WHATSAPP_NUMBER_ENV = os.getenv("ID_WHATSAPP_NUMBER")
+API_VERSION_WHATSAPP_ENV = os.getenv("API_VERSION_WHATSAPP")
+
 
 class MenssageLeft(GenericAPIView, ResponseMixin):
     serializer_class = SignupSerializers
 
     def get(self, request, *args, **kwargs):
-
         try:
-            filter = request.GET['filter']
+            chat_filter = request.GET["filter"]
 
-            if filter != '':
+            if chat_filter != "":
                 filters = " (CONCAT(p.nombre, ' ', p.segundonombre, ' ', p.apellido) LIKE %s OR p.telefonowhatsapp LIKE %s)"
-                filters_values   = ['%' + filter + '%', '%' + filter + '%']
-            else: 
-                get_status = request.GET['list']
+                filters_values = ["%" + chat_filter + "%", "%" + chat_filter + "%"]
+            else:
+                get_status = request.GET["list"]
                 filters = "  c.estado_id = %s "
-                filters_values   = get_status
+                filters_values = get_status
 
             with connection.cursor() as cursor:
-                cursor.execute("""
+                cursor.execute(
+                    """
                     SELECT
                         subq.recipiente_id,
                         subq.type_message,
@@ -65,142 +73,167 @@ class MenssageLeft(GenericAPIView, ResponseMixin):
                             CONCAT(UPPER(LEFT(ma.nombre, 1)), LCASE(SUBSTRING(ma.nombre, 2))) as message_status
                         FROM mensajeria m
                         INNER JOIN conversaciones c ON (m.conversacion_id = c.id)
-                        INNER JOIN destinatarios d ON (m.destinatario_id = d.id)
+                        INNER JOIN destinatarios d ON (c.destinatario_id = d.id)
                         INNER JOIN personas p ON (d.persona_id = p.id)
                         LEFT JOIN maestras ma on (m.estado_id = ma.id)
-                        WHERE """ + filters + """
+                        WHERE """
+                    + filters
+                    + """
                         AND DATE_FORMAT(FROM_UNIXTIME(m.timestamp_w), '%%Y-%%m-%%d') >= CURDATE() - INTERVAL 300 DAY
                     ) AS subq
                     WHERE subq.row_num = 1;
-                """, filters_values)
+                """,
+                    filters_values,
+                )
 
-                
                 rows = cursor.fetchall()
 
             chats = []
             for row in rows:
-                
-                recipiente_id       = row[0]
-                type_message        = row[1]
-                texto               = row[2]
-                timestamp_date      = row[3]
-                timestamp_hour      = row[4]
-                name_user           = row[5]
-                last_name_user      = row[6]
-                id_message_status   = row[7]
-                message_status      = row[8]
+                recipiente_id = row[0]
+                type_message = row[1]
+                texto = row[2]
+                timestamp_date = row[3]
+                timestamp_hour = row[4]
+                name_user = row[5]
+                last_name_user = row[6]
+                id_message_status = row[7]
+                message_status = row[8]
 
                 chat = {
-                    'recipiente_id'     :    recipiente_id,
-                    'type_message'      :    type_message,
-                    'texto'             :    texto,
-                    'timestamp_date'    :    timestamp_date,
-                    'timestamp_hour'    :    timestamp_hour,
-                    'name_user'         :    name_user,
-                    'last_name_user'    :    last_name_user,
-                    'id_message_status' :    id_message_status,
-                    'message_status'    :    message_status,
+                    "recipiente_id": recipiente_id,
+                    "type_message": type_message,
+                    "texto": texto,
+                    "timestamp_date": timestamp_date,
+                    "timestamp_hour": timestamp_hour,
+                    "name_user": name_user,
+                    "last_name_user": last_name_user,
+                    "id_message_status": id_message_status,
+                    "message_status": message_status,
                 }
                 chats.append(chat)
 
-            data = {"chats" : chats}
+            data = {"chats": chats}
 
             self.data = {"status": status.HTTP_200_OK, "data": data, "state": True}
             return Response(self.response_obj)
         except Exception as e:
-            response_data = {
-            }
-            
+            response_data = {}
+
             self.data = {"status": status.HTTP_404_NOT_FOUND, "data": {}, "state": True}
             return Response(self.response_obj)
+
 
 class MenssageFind(CreateAPIView, ResponseMixin):
     serializer_class = SignupSerializers
 
     def get(self, request, *args, **kwargs):
-        
         try:
-            telefonowhatsapp = request.GET.get('user_phone')
+            telefonowhatsapp = request.GET.get("user_phone")
 
             persona = Personas.objects.filter(telefonowhatsapp=telefonowhatsapp).first()
 
             if persona:
-                persona_data = json.loads(serializers.serialize('json', [persona]))[0]  
+                persona_data = json.loads(serializers.serialize("json", [persona]))[0]
 
-                mensajes = Mensajeria.objects.filter(
-                recipiente_id=telefonowhatsapp,
-                created_at__gte=timezone.now() - timezone.timedelta(weeks=200)
-                ).annotate(
-                    estado_annotation=Case(
-                        When(created_by_id__isnull=True, then=Value('recibido')),
-                        default=Value('enviado'),
-                        output_field=CharField()
-                    ),
-                    fecha=TruncDate('created_at')
-                ).values('estado_annotation', 'recipiente_id', 'fecha', 
-                        'estado_id', 'timestamp_w', 'texto', 'mensaje_id', 
-                        'mime_type', 'link', 'filename', 'voice', 'id').annotate(
-                    cantidad_registros=Count('id')
-                ).order_by('fecha')
+                mensajes = (
+                    Mensajeria.objects.filter(
+                        recipiente_id=telefonowhatsapp,
+                        created_at__gte=timezone.now() - timezone.timedelta(weeks=200),
+                    )
+                    .annotate(
+                        estado_annotation=Case(
+                            When(created_by_id__isnull=True, then=Value("recibido")),
+                            default=Value("enviado"),
+                            output_field=CharField(),
+                        ),
+                        fecha=TruncDate("created_at"),
+                    )
+                    .values(
+                        "estado_annotation",
+                        "recipiente_id",
+                        "fecha",
+                        "estado_id",
+                        "timestamp_w",
+                        "texto",
+                        "mensaje_id",
+                        "mime_type",
+                        "link",
+                        "filename",
+                        "voice",
+                        "id",
+                    )
+                    .annotate(cantidad_registros=Count("id"))
+                    .order_by("fecha")
+                )
 
                 resultados = {}
                 for mensaje in mensajes:
                     # fecha = mensaje['fecha'].strftime('%Y-%m-%d')
-                    fecha_int = float(mensaje['timestamp_w'])
+                    fecha_int = float(mensaje["timestamp_w"])
                     fecha_datetime = timezone.datetime.fromtimestamp(fecha_int)
-                    fecha = fecha_datetime.strftime('%Y/%m/%d')
+                    fecha = fecha_datetime.strftime("%Y/%m/%d")
 
-
-                    timestamp = float(mensaje['timestamp_w'])
+                    timestamp = float(mensaje["timestamp_w"])
                     fecha_hora = datetime.fromtimestamp(timestamp)
-                    hora = fecha_hora.strftime('%H')
-                    minutos = fecha_hora.strftime('%M')
-                    hora_completa = hora + ':' + minutos
-
+                    hora = fecha_hora.strftime("%H")
+                    minutos = fecha_hora.strftime("%M")
+                    hora_completa = hora + ":" + minutos
 
                     if fecha not in resultados:
                         resultados[fecha] = []
-                    resultados[fecha].append({
-                        'estado': mensaje['estado_annotation'],
-                        'recipiente_id': mensaje['recipiente_id'],
-                        'fecha': fecha,
-                        'id': mensaje['id'],
-                        'estado_id': mensaje['estado_id'],
-                        'texto': mensaje['texto'],
-                        'mensaje_id': mensaje['mensaje_id'],
-                        'mime_type': mensaje['mime_type'],
-                        'dir': mensaje['link'],
-                        'filename': mensaje['filename'],
-                        'voice': mensaje['voice'],
-                        'hora': hora_completa
-                    })
+                    resultados[fecha].append(
+                        {
+                            "estado": mensaje["estado_annotation"],
+                            "recipiente_id": mensaje["recipiente_id"],
+                            "fecha": fecha,
+                            "id": mensaje["id"],
+                            "estado_id": mensaje["estado_id"],
+                            "texto": mensaje["texto"],
+                            "mensaje_id": mensaje["mensaje_id"],
+                            "mime_type": mensaje["mime_type"],
+                            "dir": mensaje["link"],
+                            "filename": mensaje["filename"],
+                            "voice": mensaje["voice"],
+                            "hora": hora_completa,
+                        }
+                    )
                 now = timezone.now()
                 limite_tiempo = now - timedelta(hours=24)
 
                 registros = Mensajeria.objects.filter(
-                    recipiente_id= telefonowhatsapp,
-                    created_at__gte=limite_tiempo
+                    recipiente_id=telefonowhatsapp, created_at__gte=limite_tiempo
                 )
 
                 cantidad_registros = True
-                if(registros.count() <= 0 ):
+                if registros.count() <= 0:
                     cantidad_registros = False
 
                 # Devolver la respuesta JSON
                 response_data = {
-                "resultados": resultados,
-                "chat_view": cantidad_registros
+                    "resultados": resultados,
+                    "chat_view": cantidad_registros,
                 }
 
-
-                
-                response_data = {"status": status.HTTP_200_OK, "data": response_data, "state": True}
+                response_data = {
+                    "status": status.HTTP_200_OK,
+                    "data": response_data,
+                    "state": True,
+                }
                 return Response(response_data)
             else:
-                response_data = {"status": status.HTTP_404_NOT_FOUND, "message": "No tienes acceso.", "state": False}
+                response_data = {
+                    "status": status.HTTP_404_NOT_FOUND,
+                    "message": "No tienes acceso.",
+                    "state": False,
+                }
                 return Response(response_data)
         except Exception as e:
-            response_data = {"status": status.HTTP_404_NOT_FOUND, "message": "No tienes acceso.", "state": False}
+            response_data = {
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "No tienes acceso.",
+                "state": False,
+            }
             return Response(response_data)
 
 
@@ -208,75 +241,74 @@ class MenssageSend(CreateAPIView, ResponseMixin):
     serializer_class = SignupSerializers
 
     def get_payload(self, whatsapp, text, type):
-        if type == 'text':
+        if type == "text":
             return {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": "57"+whatsapp,
-            "type": "text",
-            "text": {
-                "preview_url": False,
-                "body": text
+                "messaging_product": "whatsapp",
+                "recipient_type": "individual",
+                "to": "57" + whatsapp,
+                "type": "text",
+                "text": {"preview_url": False, "body": text},
             }
-        }
 
-    def post_conversation(recipient_id):
+    def post_conversation(self, recipient_id):
         conversacion = Conversaciones.objects.update_or_create(
-            destinatario_id=recipient_id,
-            defaults={
-                "estado_id": 758
-            }
+            destinatario_id=recipient_id, defaults={"estado_id": 758}
         )
         return conversacion.id
-    
-    
-    
+
     def post(self, request, *args, **kwargs):
-
         # try:
-            info = json.loads(request.body) 
-            recipient    = info[0]['recipient']
-            message      = info[0]['message']
-            type         = info[0]['type']
+        info = json.loads(request.body)
+        recipient = info[0]["recipient"]
+        message = info[0]["message"]
+        type = info[0]["type"]
 
-            user = request.user
-            data = {
-                "recipient" : recipient,
-                "message"   : message,
-                "user_id"   : user.id,
-                }
+        user = request.user
+        data = {
+            "recipient": recipient,
+            "message": message,
+            "user_id": user.id,
+        }
+
+        persona_model = Personas.objects.get(telefonowhatsapp=recipient)
+        recipient_model = Destinatarios.objects.get(persona=persona_model)
+        user_whatsapp = persona_model.telefonowhatsapp
+        recipient_id = recipient_model.id
+        if persona_model:
             
-            persona_model       =   Personas.objects.get(telefonowhatsapp=recipient)
-            recipient_model     =   Destinatarios.objects.get(persona=persona_model)
-            user_whatsapp       = persona_model.telefonowhatsapp
-            recipient_id        = recipient_model.id
-            if(persona_model):
+            user_whatsapp = persona_model.telefonowhatsapp
 
-                user_whatsapp = persona_model.telefonowhatsapp
-                
-                url = 'https://graph.facebook.com/'+API_VERSION_WHATSAPP_ENV+'/'+ID_WHATSAPP_NUMBER_ENV+'/messages'
-                headers = {
-                    'Authorization': API_KEY_ENV,
-                    'Content-Type': 'application/json'
-                }
+            url = (
+                "https://graph.facebook.com/"
+                + API_VERSION_WHATSAPP_ENV
+                + "/"
+                + ID_WHATSAPP_NUMBER_ENV
+                + "/messages"
+            )
+            headers = {"Authorization": API_KEY_ENV, "Content-Type": "application/json"}
 
-                payload = self.get_payload(recipient, message, type)
+            payload = self.get_payload(recipient, message, type)
 
-                response = requests.post(url, headers=headers, json=payload)
-                response_json = response.json()
+            response = requests.post(url, headers=headers, json=payload)
+            response_json = response.json()
 
-                # conversacion_id = self.post_conversation()
+            # conversacion_id = self.post_conversation()
 
+            self.data = {
+                "status": status.HTTP_200_OK,
+                "data": {"payload": payload},
+                "state": True,
+            }
 
+            return Response(self.response_obj)
+        else:
+            response_data = {
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Error destinatario no valido",
+                "state": False,
+            }
+            return Response(response_data)
 
-                self.data = {"status": status.HTTP_200_OK, 
-                             "data": { 
-                                 "payload" : payload
-                             }, "state": True}
-                return Response(self.response_obj)
-            else:
-                response_data = {"status": status.HTTP_404_NOT_FOUND, "message": "Error destinatario no valido", "state": False}
-                return Response(response_data)
-        # except Exception as e:
-        #     response_data = {"status": status.HTTP_404_NOT_FOUND, "message": "error", "state": False}
-        #     return Response(response_data)
+    # except Exception as e:
+    #     response_data = {"status": status.HTTP_404_NOT_FOUND, "message": "error", "state": False}
+    #     return Response(response_data)
--- mensajeria/views/chat/urls.py
+++ mensajeria/views/chat/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .chat import MenssageLeft, MenssageFind, MenssageSend
 
 urlpatterns = [
-    path("message_left/",  MenssageLeft.as_view(), name="message_left"),
-    path("message_find/",  MenssageFind.as_view(), name="message_find"),
-    path("message_send/",  MenssageSend.as_view(), name="message_send"),
-
+    path("message_left/", MenssageLeft.as_view(), name="message_left"),
+    path("message_find/", MenssageFind.as_view(), name="message_find"),
+    path("message_send/", MenssageSend.as_view(), name="message_send"),
 ]
