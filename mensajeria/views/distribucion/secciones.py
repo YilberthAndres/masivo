59351c59506572a0d6db45b7fdd8e96f02b746a2
Nelson <neldecas12@gmail.com>
2023-12-20 10:28:43 -0500

    serializers

--- mensajeria/views/distribucion/secciones.py
+++ mensajeria/views/distribucion/secciones.py
@@ -8,26 +8,27 @@ from django.db.models import Q
 from django.db.models import Prefetch
 from .serializer import SeccionesSerializer
 
+
 class SeccionesDistribucion(CreateAPIView, ResponseMixin):
     serializer_class = SignupSerializers
 
     def post(self, request, *args, **kwargs):
         try:
-            data            = request.data[0]
-            name            = data.get("name", None)
-            description     = data.get("description", None)
-            area_id            = data.get("area_id", None)
-            user            = request.user
-            
-            
+            data = request.data[0]
+            name = data.get("name", None)
+            description = data.get("description", None)
+            area_id = data.get("area_id", None)
+            user = request.user
 
-            if(name == ''):
+            if name == "":
                 self.status = status.HTTP_400_BAD_REQUEST
                 self.error = "El nombre es un campo requerido."
             else:
                 name = name.strip().lower()
-                seccion_existente   = Secciones.objects.filter(Q(nombre__iexact=name)).first()
-                area_existente      = Areas.objects.filter(Q(id = area_id)).first()
+                seccion_existente = Secciones.objects.filter(
+                    Q(nombre__iexact=name)
+                ).first()
+                area_existente = Areas.objects.filter(Q(id=area_id)).first()
 
                 if seccion_existente:
                     self.status = status.HTTP_400_BAD_REQUEST
@@ -36,12 +37,12 @@ class SeccionesDistribucion(CreateAPIView, ResponseMixin):
                     self.status = status.HTTP_400_BAD_REQUEST
                     self.error = "Debe agregar un area valida."
                 else:
-                    new_seccion                = Secciones()
-                    new_seccion.nombre         = name
-                    new_seccion.descripcion    = description
-                    new_seccion.area_id        = area_id
-                    new_seccion.estado_id      = 596
-                    new_seccion.created_by     = user
+                    new_seccion = Secciones()
+                    new_seccion.nombre = name
+                    new_seccion.descripcion = description
+                    new_seccion.area_id = area_id
+                    new_seccion.estado_id = 596
+                    new_seccion.created_by = user
                     new_seccion.save()
 
             return Response(self.response_obj)
@@ -51,18 +52,20 @@ class SeccionesDistribucion(CreateAPIView, ResponseMixin):
             return Response(self.response_obj)
 
     def get(self, request, *args, **kwargs):
-
         try:
-            secciones_consulta =  (
+            secciones_consulta = (
                 Secciones.objects.filter(estado_id=596)
                 .select_related("estado", "created_by")
-                .prefetch_related(Prefetch("grupos_set", queryset = Grupos.objects.filter(
-                            estado_id=596
-                        ).select_related("estado", "created_by")
+                .prefetch_related(
+                    Prefetch(
+                        "grupos_set",
+                        queryset=Grupos.objects.filter(estado_id=596).select_related(
+                            "estado", "created_by"
+                        ),
                     )
                 )
             )
-            
+
             result_secciones = SeccionesSerializer(secciones_consulta, many=True)
 
             self.status = status.HTTP_200_OK
@@ -75,22 +78,21 @@ class SeccionesDistribucion(CreateAPIView, ResponseMixin):
             return Response(self.response_obj)
 
     def put(self, request, *args, **kwargs):
-        try: 
-            
-            data            =   request.data[0]
-            name            =   data.get("name", None)
-            description     =   data.get("description", None)
-            status_id       =   data.get("status_id", None)
-            area_id         =   data.get("area_id", None)
-            user            =   request.user
-            seccion_id      =   self.kwargs.get("seccion_id", None)
-            seccion         =   Secciones.objects.get(id=seccion_id)
+        try:
+            data = request.data[0]
+            name = data.get("name", None)
+            description = data.get("description", None)
+            status_id = data.get("status_id", None)
+            area_id = data.get("area_id", None)
+            user = request.user
+            seccion_id = self.kwargs.get("seccion_id", None)
+            seccion = Secciones.objects.get(id=seccion_id)
 
-            seccion.nombre         = name
-            seccion.descripcion    = description
-            seccion.estado_id      = status_id
-            seccion.area_id        = area_id
-            seccion.updated_by     = user
+            seccion.nombre = name
+            seccion.descripcion = description
+            seccion.estado_id = status_id
+            seccion.area_id = area_id
+            seccion.updated_by = user
             seccion.save()
 
             return Response(self.response_obj)
@@ -98,39 +100,41 @@ class SeccionesDistribucion(CreateAPIView, ResponseMixin):
             self.status = status.HTTP_400_BAD_REQUEST
             self.error = e.args
             return Response(self.response_obj)
-        
 
     def delete(self, request, *args, **kwargs):
         try:
-            user                =   request.user
-            seccion_id          =   self.kwargs.get("seccion_id", None)
-            seccion             =   Secciones.objects.get(id=seccion_id)
-            seccion.estado_id   =   597
-            seccion.updated_by  =   user
+            user = request.user
+            seccion_id = self.kwargs.get("seccion_id", None)
+            seccion = Secciones.objects.get(id=seccion_id)
+            seccion.estado_id = 597
+            seccion.updated_by = user
             seccion.save()
             return Response(self.response_obj)
         except Secciones.DoesNotExist as e:
             self.status = status.HTTP_400_BAD_REQUEST
             self.error = e.args
             return Response(self.response_obj)
-        
+
+
 class SeccionesFind(CreateAPIView, ResponseMixin):
-    serializer_class = SignupSerializers   
+    serializer_class = SignupSerializers
 
     def get(self, request, *args, **kwargs):
-
-        try: 
-            seccion_id =   self.kwargs.get("seccion_id", None)
-            secciones_consulta =  (
+        try:
+            seccion_id = self.kwargs.get("seccion_id", None)
+            secciones_consulta = (
                 Secciones.objects.filter(id=seccion_id)
                 .select_related("estado", "created_by")
-                .prefetch_related(Prefetch("grupos_set", queryset = Grupos.objects.filter(
-                            estado_id=596
-                        ).select_related("estado", "created_by")
+                .prefetch_related(
+                    Prefetch(
+                        "grupos_set",
+                        queryset=Grupos.objects.filter(estado_id=596).select_related(
+                            "estado", "created_by"
+                        ),
                     )
                 )
             )
-            
+
             result_secciones = SeccionesSerializer(secciones_consulta, many=True)
 
             self.status = status.HTTP_200_OK
@@ -141,4 +145,3 @@ class SeccionesFind(CreateAPIView, ResponseMixin):
             self.status = status.HTTP_400_BAD_REQUEST
             self.error = e.args
             return Response(self.response_obj)
-        
--- mensajeria/views/distribucion/serializer.py
+++ mensajeria/views/distribucion/serializer.py
@@ -3,44 +3,66 @@ from mensajeria.models import Areas, Secciones, Grupos
 
 
 class GruposSerializer(serializers.Serializer):
-    id              = serializers.PrimaryKeyRelatedField(read_only=True)
-    nombre          = serializers.PrimaryKeyRelatedField(read_only=True)
-    descripcion     = serializers.PrimaryKeyRelatedField(read_only=True)
-    estado_id       = serializers.PrimaryKeyRelatedField(read_only=True)
-    estado_nombre   = serializers.ReadOnlyField(source='estado.nombre')
-    seccion_id      = serializers.PrimaryKeyRelatedField(read_only=True)
-    seccion_nombre  = serializers.ReadOnlyField(source='seccion.nombre')
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    nombre = serializers.CharField(read_only=True)
+    descripcion = serializers.CharField(read_only=True)
+    estado_id = serializers.CharField(read_only=True)
+    estado_nombre = serializers.ReadOnlyField(source="estado.nombre")
+    seccion_id = serializers.CharField(read_only=True)
+    seccion_nombre = serializers.ReadOnlyField(source="seccion.nombre")
     # created_at      = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-    created_by      = serializers.ReadOnlyField(source='created_by.id')
+    created_by = serializers.ReadOnlyField(source="created_by.id")
 
     class Meta:
         model = Grupos
-    
+
 
 class SeccionesSerializer(serializers.Serializer):
-    id              = serializers.PrimaryKeyRelatedField(read_only=True)
-    nombre          = serializers.PrimaryKeyRelatedField(read_only=True)
-    descripcion     = serializers.PrimaryKeyRelatedField(read_only=True)
-    estado_id       = serializers.PrimaryKeyRelatedField(read_only=True)
-    estado_nombre   = serializers.ReadOnlyField(source='estado.nombre')
-    area_id         = serializers.PrimaryKeyRelatedField(read_only=True)
-    area_nombre     = serializers.ReadOnlyField(source='area.nombre')
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    nombre = serializers.CharField(read_only=True)
+    descripcion = serializers.CharField(read_only=True)
+    estado_id = serializers.CharField(read_only=True)
+    estado_nombre = serializers.ReadOnlyField(source="estado.nombre")
+    area_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    area_nombre = serializers.ReadOnlyField(source="area.nombre")
     # created_at      = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-    created_by      = serializers.ReadOnlyField(source='created_by.id')
-    grupos_set      = GruposSerializer(read_only=True, many=True)
+    created_by = serializers.ReadOnlyField(source="created_by.id")
+    grupos_set = GruposSerializer(read_only=True, many=True)
+
+    def to_representation(self, instance):
+        representation = super(SeccionesSerializer, self).to_representation(instance)
+        representation["grupos"] = representation["grupos_set"]
+
+        field_to_show = [
+            "id",
+            "nombre",
+            "descripcion",
+            "estado_id",
+            "estado_nombre",
+            "area_id",
+            "area_nombre",
+            "created_by",
+        ]
+
+        return {
+            field: representation[field]
+            for field in field_to_show
+            if field in representation
+        }
 
     class Meta:
         model = Secciones
 
+
 class AreasSerializer(serializers.Serializer):
-    id              = serializers.PrimaryKeyRelatedField(read_only=True)
-    nombre          = serializers.PrimaryKeyRelatedField(read_only=True)
-    descripcion     = serializers.PrimaryKeyRelatedField(read_only=True)
-    estado_id       = serializers.PrimaryKeyRelatedField(read_only=True)
-    estado_nombre   = serializers.ReadOnlyField(source='estado.nombre')
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    nombre = serializers.CharField(read_only=True)
+    descripcion = serializers.CharField(read_only=True)
+    estado_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    estado_nombre = serializers.ReadOnlyField(source="estado.nombre")
     # created_at      = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-    created_by      = serializers.ReadOnlyField(source='created_by.id')
-    secciones_set   = SeccionesSerializer(read_only=True, many=True)
+    created_by = serializers.ReadOnlyField(source="created_by.id")
+    secciones_set = SeccionesSerializer(read_only=True, many=True)
 
     class Meta:
         model = Areas
